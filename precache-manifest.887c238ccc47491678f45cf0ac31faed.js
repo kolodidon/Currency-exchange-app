3e115d15e3037c3024bc705a4cce60d2e1801591
kolodidon <www.teteva@yandex.ru>
2021-03-22 16:16:06 +0300

    Updates

---
+++ precache-manifest.887c238ccc47491678f45cf0ac31faed.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018ee8ecaa7a1a2ee6276f6931e74cb1",
+    "url": "/exchange-app/index.html"
+  },
+  {
+    "revision": "220332fbced0c86d6344",
+    "url": "/exchange-app/static/css/main.b589e6bf.chunk.css"
+  },
+  {
+    "revision": "b72159c9d9b469eb1d2c",
+    "url": "/exchange-app/static/js/2.2017eb84.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/exchange-app/static/js/2.2017eb84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220332fbced0c86d6344",
+    "url": "/exchange-app/static/js/main.533f09d9.chunk.js"
+  },
+  {
+    "revision": "e6ef17e1727ba4293740",
+    "url": "/exchange-app/static/js/runtime-main.3d88282c.js"
+  },
+  {
+    "revision": "dc8dc7aaaa4da39106478ec24e939fbf",
+    "url": "/exchange-app/static/media/CNY.dc8dc7aa.png"
+  },
+  {
+    "revision": "e1c9768d02aff22f203c4f93d0fe670d",
+    "url": "/exchange-app/static/media/EUR.e1c9768d.png"
+  },
+  {
+    "revision": "38c1328568118ccee45db1e5b93b5e71",
+    "url": "/exchange-app/static/media/GBP.38c13285.png"
+  },
+  {
+    "revision": "9987aabc6c809425e08b51935120d51a",
+    "url": "/exchange-app/static/media/JPY.9987aabc.png"
+  },
+  {
+    "revision": "0c79b82e54d1e0e2c1ef7f03be71a687",
+    "url": "/exchange-app/static/media/RUB.0c79b82e.png"
+  },
+  {
+    "revision": "ff97e132da519724034302a4a92750ef",
+    "url": "/exchange-app/static/media/USD.ff97e132.png"
+  }
+]);
